16d2febeeb08cb046327b2461f0343148434f90c
John Drago <jdrago.999@gmail.com>
2015-12-18 10:33:28 -0800

    Added tests for phase finished handler

--- spec/distribot/phase_finished_handler_spec.rb
+++ spec/distribot/phase_finished_handler_spec.rb
@@ -1,10 +1,6 @@
 require 'spec_helper'
 
 describe Distribot::PhaseFinishedHandler do
-  before do
-    Distribot.stub(:subscribe)
-    Distribot.stub(:publish!)
-  end
   describe 'definition' do
     it 'subscribes to the correct queue' do
       expect(Distribot::Handler.queue_for(described_class)).to eq 'distribot.workflow.phase.finished'
@@ -13,47 +9,45 @@ describe Distribot::PhaseFinishedHandler do
       expect(Distribot::Handler.handler_for(described_class)).to eq :callback
     end
     it 'has a method matching the handler name' do
+      expect(Distribot).to receive(:subscribe)
       expect(described_class.new).to respond_to :callback
     end
   end
 
   describe '#callback( :workflow_id, :phase )' do
     before do
-      @workflow_attrs = {
-        name: 'finisher',
-        phases: [
-          {name: 'start', is_initial: true, transitions_to: 'phase1'},
-        ]
-      }
+      expect(Distribot).to receive(:subscribe)
     end
     context 'when workflow is' do
+      before do
+        @workflow = double('workflow')
+        @workflow_id = 'xxx'
+        expect(Distribot::Workflow).to receive(:find).with(@workflow_id) { @workflow }
+      end
       context 'still in :phase' do
+        before do
+          expect(@workflow).to receive(:current_phase){ 'start' }
+        end
         context 'and the workflow' do
           context 'has a next phase' do
             before do
-              @workflow_attrs[:phases] << {
-                name: 'phase1'
-              }
-              @workflow = Distribot::Workflow.create!(@workflow_attrs)
-              expect(Distribot::Workflow).to receive(:find) { @workflow }
+              expect(@workflow).to receive(:next_phase){ 'finish' }
             end
             it 'tells the workflow to transition to the next phase' do
-              expect(@workflow).to receive(:transition_to!).with('phase1')
-              described_class.new.callback(workflow_id: @workflow.id, phase: 'start')
+              expect(@workflow).to receive(:transition_to!).with('finish')
+              described_class.new.callback(workflow_id: @workflow_id, phase: 'start')
             end
           end
           context 'does not have a next phase' do
             before do
-              @workflow_attrs[:phases].first.delete(:transitions_to)
-              @workflow_attrs[:phases].first[:is_final] = true
-              @workflow = Distribot::Workflow.create!(@workflow_attrs)
-              expect(Distribot::Workflow).to receive(:find) { @workflow }
+              expect(@workflow).to receive(:next_phase){ nil }
+              expect(@workflow).to receive(:id){ @workflow_id }
             end
             it 'publishes to distribot.workflow.finished' do
               expect(Distribot).to receive(:publish!).with('distribot.workflow.finished', {
-                workflow_id: @workflow.id
+                workflow_id: @workflow_id
               })
-              described_class.new.callback(workflow_id: @workflow.id, phase: 'start')
+              described_class.new.callback(workflow_id: @workflow_id, phase: 'start')
             end
           end
         end
