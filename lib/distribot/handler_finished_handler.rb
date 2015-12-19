04b465ef4ab036475458195d1b78e69a09843181
John Drago <jdrago.999@gmail.com>
2015-12-19 12:27:59 -0800

    More rubocop updates

--- lib/distribot/handler_finished_handler.rb
+++ lib/distribot/handler_finished_handler.rb
@@ -1,30 +1,28 @@
 
 module Distribot
-
   class HandlerFinishedHandler
     include Distribot::Handler
     subscribe_to 'distribot.workflow.handler.finished', handler: :callback
 
     def callback(message)
       workflow = Distribot::Workflow.find(message[:workflow_id])
-      phase = workflow.phase( message[:phase] )
+      phase = workflow.phase(message[:phase])
 
-      if self.all_phase_handler_tasks_are_complete?(workflow, phase)
-        Distribot.publish! 'distribot.workflow.phase.finished', {
-          workflow_id: workflow.id,
-          phase: phase.name
-        }
-      end
+      Distribot.publish!(
+        'distribot.workflow.phase.finished',
+        workflow_id: workflow.id,
+        phase: phase.name
+      ) if self.all_phase_handler_tasks_are_complete?(workflow, phase)
     end
 
-    # TODO: Consider folding this logic somewhere else where it makes more sense.
     def all_phase_handler_tasks_are_complete?(workflow, phase)
       redis = Distribot.redis
+      name = phase.name
       phase.handlers
-        .map{|handler| "distribot.workflow.#{workflow.id}.#{phase.name}.#{handler}.tasks" }
-        .map{|task_counter_key| redis.get(task_counter_key) }
+        .map { |h| "distribot.workflow.#{workflow.id}.#{name}.#{h}.tasks" }
+        .map { |task_counter_key| redis.get(task_counter_key) }
         .reject(&:nil?)
-        .select{|val| val.to_i > 0 }
+        .select { |val| val.to_i > 0 }
         .empty?
     end
   end
