6f953fd3916f66f62babe4731b1645fc14ff87e1
John Drago <jdrago.999@gmail.com>
2015-12-18 23:06:26 -0800

    Extracted method.

--- lib/distribot/phase_started_handler.rb
+++ lib/distribot/phase_started_handler.rb
@@ -17,34 +17,37 @@ module Distribot
         }
       else
         phase.handlers.each do |handler|
-          worker_version = if handler.version
-            wanted_version = Gem::Dependency.new(handler.to_s, handler.version)
-            # Figure out the highest acceptable version of the handler we can assign work to:
-            self.handler_versions(handler.to_s)
-              .reverse
-              .select{|x| wanted_version.match?(handler.to_s, x.to_s) }
-              .first
-              .to_s
-          else
-            # Find the highest version for this queue:
-            self.handler_versions(handler.to_s).last
-          end
-          raise "Cannot find suitable #{handler} version #{handler.version}" unless worker_version
-
-          enumerate_queue = "distribot.workflow.handler.#{handler}.#{worker_version}.enumerate"
-          task_queue = "distribot.workflow.handler.#{handler}.#{worker_version}.tasks"
-          finished_queue = "distribot.workflow.task.finished"
-          task_counter = "distribot.workflow.#{workflow.id}.#{phase.name}.#{handler}.finished"
-
-          Distribot.publish! enumerate_queue, {
-            workflow_id: workflow.id,
-            phase: phase.name,
-            task_queue: task_queue,
-            task_counter: task_counter,
-            finished_queue: finished_queue
-          }
+          jumpstart_handler(workflow, phase, handler)
         end
       end
+    end
+
+    def jumpstart_handler(workflow, phase, handler)
+      worker_version = if handler.version
+        wanted_version = Gem::Dependency.new(handler.to_s, handler.version)
+        # Figure out the highest acceptable version of the handler we can assign work to:
+        self.handler_versions(handler.to_s)
+          .reverse
+          .find{|x| wanted_version.match?(handler.to_s, x.to_s) }
+          .to_s
+      else
+        # Find the highest version for this queue:
+        self.handler_versions(handler.to_s).last
+      end
+      raise "Cannot find suitable #{handler} version #{handler.version}" unless worker_version
+
+      enumerate_queue = "distribot.workflow.handler.#{handler}.#{worker_version}.enumerate"
+      task_queue = "distribot.workflow.handler.#{handler}.#{worker_version}.tasks"
+      finished_queue = "distribot.workflow.task.finished"
+      task_counter = "distribot.workflow.#{workflow.id}.#{phase.name}.#{handler}.finished"
+
+      Distribot.publish! enumerate_queue, {
+        workflow_id: workflow.id,
+        phase: phase.name,
+        task_queue: task_queue,
+        task_counter: task_counter,
+        finished_queue: finished_queue
+      }
     end
 
     def handler_versions(handler)
