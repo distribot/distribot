57809f451845421aea62bd0abc61a71c4cb87165
John Drago <jdrago.999@gmail.com>
2015-12-19 12:32:10 -0800

    Rubocop is my hero.

--- lib/distribot/phase.rb
+++ lib/distribot/phase.rb
@@ -9,34 +9,38 @@ module Distribot
                   :on_error_transition_to,
                   :handlers
 
-    def initialize(attrs={})
-      attrs.each do |key,val|
+    def initialize(attrs = {})
+      attrs.each do |key, val|
         next if key.to_s == 'handlers'
-        self.send("#{key}=", val)
+        public_send("#{key}=", val)
       end
       self.name = name
-      self.handlers = [ ]
-      attrs[:handlers] ||= [ ]
-      attrs[:handlers].each do |handler|
-        if handler.is_a? Hash
-          self.handlers.push( PhaseHandler.new handler )
-        else
-          self.handlers.push( PhaseHandler.new name: handler )
-        end
-      end
+      self.handlers = []
+      initialize_handlers(attrs[:handlers] || [])
     end
 
     def to_hash
       {
-        id: self.id,
-        name: self.name,
-        is_initial: self.is_initial || false,
-        is_final: self.is_final || false,
-        transitions_to: self.transitions_to,
-        on_error_transition_to: self.on_error_transition_to,
-        handlers: self.handlers || [ ]
+        id: id,
+        name: name,
+        is_initial: is_initial || false,
+        is_final: is_final || false,
+        transitions_to: transitions_to,
+        on_error_transition_to: on_error_transition_to,
+        handlers: handlers
       }
     end
 
+    private
+
+    def initialize_handlers(handler_args)
+      handler_args.each do |handler|
+        if handler.is_a? Hash
+          handlers.push(PhaseHandler.new handler)
+        else
+          handlers.push(PhaseHandler.new name: handler)
+        end
+      end
+    end
   end
 end
