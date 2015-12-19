2a28328d2c05fb6e653e5008dfefbcf776fd7217
John Drago <jdrago.999@gmail.com>
2015-12-18 17:23:58 -0800

    Offlined tests for workflow finished handler

--- spec/distribot/workflow_finished_handler_spec.rb
+++ spec/distribot/workflow_finished_handler_spec.rb
@@ -1,9 +1,6 @@
 require 'spec_helper'
 
 describe Distribot::WorkflowFinishedHandler do
-  before :each do
-    Distribot.stub(:publish!)
-  end
   describe 'definition' do
     it 'subscribes to the distribot.workflow.finished queue' do
       expect(Distribot::Handler.queue_for(described_class)).to eq 'distribot.workflow.finished'
@@ -12,36 +9,24 @@ describe Distribot::WorkflowFinishedHandler do
       expect(Distribot::Handler.handler_for(described_class)).to eq :callback
     end
     it 'has a method matching the handler name' do
+      expect(Distribot).to receive(:subscribe)
       expect(described_class.new).to respond_to :callback
     end
   end
 
   describe '#callback' do
     before do
-      @workflow = Distribot::Workflow.create!(
-        name: 'test-workflow',
-        phases: [
-          {name: 'start', is_initial: true, transitions_to: 'working'},
-          {name: 'working', handlers: ['ExampleHandler'], transitions_to: 'finished'},
-          {name: 'finished', is_final: true}
-        ]
-      )
-      @workflow_id = @workflow.id
+      @message = {
+        workflow_id: 'xxx'
+      }
+      redis = double('redis')
+      expect(redis).to receive(:decr).with('distribot.workflows.running')
+      expect(redis).to receive(:srem).with('distribot.workflows.active', @message[:workflow_id])
+      expect(Distribot).to receive(:redis).exactly(2).times{ redis }
+      expect(Distribot).to receive(:subscribe)
     end
-    context 'exists' do
-      before do
-        @queue_name = "distribot.workflow.#{@workflow_id}.finished.callback"
-        expect(Distribot::Workflow).to receive(:find).with(@workflow_id){ @workflow }
-      end
-      it 'decrements the running tally of how many workflows are currently in process' do
-        redis_decr = double('redis-decr')
-        expect(redis_decr).to receive(:decr).with('distribot.workflows.running')
-        expect(Distribot).to receive(:redis).ordered{ redis_decr }
-        redis_srem = double('redis-srem')
-        expect(redis_srem).to receive(:srem).with('distribot.workflows.active', @workflow.id)
-        expect(Distribot).to receive(:redis).ordered{ redis_srem }
-        described_class.new.callback(workflow_id: @workflow_id)
-      end
+    it 'decrements the running tally of how many workflows are currently in process' do
+      described_class.new.callback(@message)
     end
   end
 end
